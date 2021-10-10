a25c6811c6ec2f1586b68b84561895c14164a203
Antara-Risha <antararisha@iut-dhaka.edu>
2021-10-10 12:11:21 +0600

    blade file for signup and login added

---
+++ BubbleShop/resources/views/login.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="../frontend/css/forms.css">
+</head>
+<body>
+    <form>
+        <h1>Login</h1>
+        <input type="email" name="" placeholder="Email">
+        <input type="password" name="" placeholder="Password">
+        
+
+        <button type="submit">Login</button>
+        
+
+    </form>
+</body>
