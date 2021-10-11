06317cac835ae81516f3cdfa588c026a39ecd022
SaiyaraOishi <saiyara.oishi@gmail.com>
2021-10-11 21:30:42 +0600

    Design changes to items.blade.php

--- BubbleShop/resources/views/items.blade.php
+++ BubbleShop/resources/views/items.blade.php
@@ -1,14 +1,23 @@
-<h1>ADD ITEMS</h1>
+<!DOCTYPE html>
+<html>
+<!-- <h1>ADD ITEMS</h1> -->
+<head>
+    <title>Add Items</title>
+    <link rel="stylesheet" type="text/css" href="../frontend/css/forms.css">
+</head>
+<body>
 <form action="items" method="POST">
     @csrf 
-    <input type="text" name="item_name" placeholder="Enter Item Name" ><br><br>
+    <h1>ADD ITEMS</h1>
+    <input type="text" name="item_name" placeholder="Enter Item Name" >
     <input type="text" className="form-control"  name="wash_type" list="wash_type" placeholder="Enter Washing Type">
                 <datalist id="wash_type">
                 <option value="Normal Wash"/>
                     <option value="Dry Wash"/>
                     
                     
-                </datalist><br><br>
-    <input type="number" name="unit_price" placeholder="Enter Unit Price" ><br><br>
+                </datalist>
+    <input type="number" name="unit_price" placeholder="Enter Unit Price" >
     <button type="submit">Add Item</button>
 </form>
+</body>
