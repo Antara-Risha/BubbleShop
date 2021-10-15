4c0f180e89f5051f1c4785cdbb75014d88a84c17
Antara-Risha <antararisha@iut-dhaka.edu>
2021-10-15 14:57:18 +0600

    LoginController added

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+   
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+
+    public function index()
+    {
+        return view('login');
+    }
+
+    /**
+     * Handle an authentication attempt.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        $credentials['status'] = 1;
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return redirect()->intended('home');
+        }
+
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ]);
+    }
+}
