1c2cc8fc9027be25519845d0ebb3d815f3c910d0
anikatahsin20 <anika.tahsin1320@gmail.com>
2021-10-15 11:49:12 +0600

    auth design changed

--- BubbleShop/resources/views/login.blade.php
+++ BubbleShop/resources/views/login.blade.php
@@ -1,21 +1,97 @@
 <!DOCTYPE html>
-<html>
-<head>
-    <title>Login</title>
-    <link rel="stylesheet" type="text/css" href="../frontend/css/forms.css">
-</head>
-<body>
-    <form action="check"  method="POST">
-        @csrf
-        <h1>Login</h1>
-        <input type="email" name="email" placeholder="Email">
-        <span type="text-danger">@error('email') {{$message}} @enderror</span>
-        <input type="password" name="password" placeholder="Password">
-        <span type="text-danger">@error('password') {{$message}} @enderror</span>
-        
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>::. Login | LMS .::</title>
+        <link href="{{ asset('assets/css/styles.css') }}" rel="stylesheet" />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
+    </head>
+    <body class="bg-primary">
+        <div id="layoutAuthentication">
+            <div id="layoutAuthentication_content">
+                <main>
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col-lg-5">
+                                <div class="card shadow-lg border-0 rounded-lg mt-5">
+                                    @if(Session::has('message'))
+                                        <div class="alert alert-success" role="alert">
+                                            {{ request()->session()->pull('message') }}
+                                        </div>
+                                    @endif
 
-        <button type="submit">Login</button>
-        
+                                    @if(Session::has('error'))
+                                        <div class="alert alert-danger" role="alert">
+                                            {{ request()->session()->pull('error') }}
+                                        </div>
+                                    @endif
 
-    </form>
-</body>
+                                    <div class="card-header"><h3 class="text-center font-weight-light my-4">Login</h3></div>
+                                    <div class="card-body">
+                                        <form method="POST" action="{{ route('authenticate') }}">
+                                            @csrf
+
+                                            @if(Session::has('message'))
+                                                <div class="alert alert-success" role="alert">
+                                                    {{ request()->session()->pull('message') }}
+                                                </div>
+                                            @endif
+
+                                            @if(Session::has('error'))
+                                                <div class="alert alert-danger" role="alert">
+                                                    {{ request()->session()->pull('error') }}
+                                                </div>
+                                            @endif
+
+                                            <div class="form-floating mb-3">
+                                                <input type="email" name="email" value="{{ old('email') }}" class="form-control @error('email') is-invalid @enderror" id="inputEmail" placeholder="name@example.com" />
+                                                <label for="inputEmail">Email address</label>
+                                                @error('email')
+                                                    <div class="alert alert-danger">{{ $message }}</div>
+                                                @enderror
+                                            </div>
+                                            <div class="form-floating mb-3">
+                                                <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" id="inputPassword" placeholder="Password" />
+                                                <label for="inputPassword">Password</label>
+                                                @error('password')
+                                                    <div class="alert alert-danger">{{ $message }}</div>
+                                                @enderror
+                                            </div>
+                                            <div class="d-flex align-items-center justify-content-center mt-4 mb-0">
+                                                <button type="submit" class="btn btn-primary">Login</button>
+                                            </div>
+                                        </form>
+                                    </div>
+                                    <div class="card-footer text-center py-3">
+                                        <div class="small"><a href="{{ route('registerForm') }}">Need an account? Sign up!</a></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+            </div>
+            <div id="layoutAuthentication_footer">
+                <footer class="py-4 bg-light mt-auto">
+                    <div class="container-fluid px-4">
+                        <div class="d-flex align-items-center justify-content-between small">
+                            <div class="text-muted">Laundry Management System 2021</div>
+                            <!-- <div>
+                                <a href="#">Privacy Policy</a>
+                                &middot;
+                                <a href="#">Terms &amp; Conditions</a>
+                            </div> -->
+                        </div>
+                    </div>
+                </footer>
+            </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="{{ asset('assets/js/scripts.js') }}"></script>
+    </body>
+</html>
+
